f1ee5ea59028aca4055e2b0d4e64c555d22582b1
Philo van Kemenade <philovankemenade@gmail.com>
2023-04-02 23:10:04 +0200

    improve heading styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="text-center">Hypervideo with HTML5 and Vue.js</h1>
+  <h1 class="text-center m-2">Hypervideo with HTML5 and Vue.js</h1>
   <VideoPlayer :videos="videos" />
 </template>
 
